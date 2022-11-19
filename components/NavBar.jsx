021f08d52de450fa2283e1268bb9293f1274cbc0
Davi Samuel <davisamasoa@gmail.com>
2022-11-19 00:30:06 -0300

    nav bar animation

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -7,17 +7,27 @@ export function NavBar(props) {
 			<h1 className=" flex  md:text-8xl  text-8xl font-bold tracking-wider">MOVIES</h1>
 			<nav className="">
 				<ul className="  text-white font-light tracking-wider flex gap-5 justify-center items-center ">
-					<li className={!props.index ? null : "font-bold text-amber-200"}>
+					<li
+						className={!props.index ? "transition duration-200 hover:opacity-70" : "font-bold text-amber-200"}
+					>
 						<Link href={"/"}>Início</Link>
 					</li>
-					<li className={!props.filme ? null : "font-bold text-amber-200"}>
+					<li
+						className={!props.filme ? "transition duration-200 hover:opacity-70" : "font-bold text-amber-200"}
+					>
 						<Link href={"/movies"}>Filmes</Link>
 					</li>
-					<li className={!props.serie ? null : "font-bold text-amber-200"}>
+					<li
+						className={!props.serie ? "transition duration-200 hover:opacity-70" : "font-bold text-amber-200"}
+					>
 						<Link href={"/series"}>Séries</Link>
 					</li>
 					<li>
-						<a target="blank" href="https://github.com/Davisamasoa">
+						<a
+							target="blank"
+							className="transition duration-200 hover:opacity-70"
+							href="https://github.com/Davisamasoa"
+						>
 							<i class="bi bi-github text-2xl "></i>
 						</a>
 					</li>
