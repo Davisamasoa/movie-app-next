604d84cbbf2b85eec0ed6fe620bff3c6b40eeb1a
Davi Samuel <davisamasoa@gmail.com>
2022-11-19 00:21:29 -0300

    title responsive fixed

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,16 +4,16 @@ import Link from "next/link";
 export function NavBar(props) {
 	return (
 		<header className="  text-amber-300 mb-10 flex justify-around flex-col items-center ">
-			<h1 className=" flex  md:text-8xl sm:text-6xl text-5xl font-bold tracking-wider">MOVIES</h1>
+			<h1 className=" flex  md:text-8xl  text-8xl font-bold tracking-wider">MOVIES</h1>
 			<nav className="">
 				<ul className="  text-white font-light tracking-wider flex gap-5 justify-center items-center ">
-					<li className={!props.index ? null : "font-semibold text-amber-200"}>
+					<li className={!props.index ? null : "font-bold text-amber-200"}>
 						<Link href={"/"}>Início</Link>
 					</li>
-					<li className={!props.filme ? null : "font-semibold text-amber-200"}>
+					<li className={!props.filme ? null : "font-bold text-amber-200"}>
 						<Link href={"/movies"}>Filmes</Link>
 					</li>
-					<li className={!props.serie ? null : "font-semibold text-amber-200"}>
+					<li className={!props.serie ? null : "font-bold text-amber-200"}>
 						<Link href={"/series"}>Séries</Link>
 					</li>
 					<li>
