8cdfddec812a0f6df0c9f3db9c170220cfc624c2
Davi Samuel <davisamasoa@gmail.com>
2023-03-06 13:31:17 -0300

    nav links fixed

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { elastic as Menu } from "react-burger-menu";
 import { BiCameraMovie, BiMovie, BiHomeAlt2 } from "react-icons/bi";
 import { ImSearch } from "react-icons/im";
+import { BsGithub } from "react-icons/bs";
 
 export function NavBar(props) {
 	return (
@@ -11,60 +12,60 @@ export function NavBar(props) {
 				<nav className=" text-primaryColor  flex justify-around flex-col items-center pl-4 ">
 					<ul className="  text-secondaryColor font-light tracking-wider  flex flex-col gap-8 justify-center mt-24">
 						<li
-							className={` flex items-center ${
+							className={`${
 								props.currentPage !== "index"
 									? "transition duration-200 hover:opacity-70"
 									: "font-bold text-white"
 							}`}
 						>
-							<BiHomeAlt2 size={21} className="bg-transparent" />
-							<Link className="ml-2" href={"/"}>
-								Início
+							<Link className="flex items-center" href={"/"}>
+								<BiHomeAlt2 size={21} className="bg-transparent" />
+								<span className="ml-2">Início</span>
 							</Link>
 						</li>
 						<li
-							className={` flex items-center ${
+							className={`${
 								props.currentPage !== "filme"
 									? "transition duration-200 hover:opacity-70"
 									: "font-bold text-white"
 							}`}
 						>
-							<BiCameraMovie size={21} className="bg-transparent" />
-							<Link className="ml-2" href={"/movies"}>
-								Filmes
+							<Link className="flex items-center" href={"/movies"}>
+								<BiCameraMovie size={21} className="bg-transparent" />
+								<span className="ml-2">Filmes</span>
 							</Link>
 						</li>
 						<li
-							className={` flex items-center ${
+							className={`${
 								props.currentPage !== "serie"
 									? "transition duration-200 hover:opacity-70"
 									: "font-bold text-white"
 							}`}
 						>
-							<BiMovie size={20} className="bg-transparent" />
-							<Link className="ml-2" href={"/series"}>
-								Séries
+							<Link className="flex items-center" href={"/series"}>
+								<BiMovie size={20} className="bg-transparent" />
+								<span className="ml-2">Séries</span>
 							</Link>
 						</li>
 						<li
-							className={` flex items-center ${
+							className={`${
 								props.currentPage !== "search"
 									? "transition duration-200 hover:opacity-70"
 									: "font-bold text-white"
 							}`}
 						>
-							<ImSearch size={20} className="bg-transparent" />
-							<Link className="ml-2" href={"/search"}>
-								Pesquisar
+							<Link className="flex items-center" href={"/search"}>
+								<ImSearch size={20} className="bg-transparent" />
+								<span className="ml-2">Pesquisar</span>
 							</Link>
 						</li>
 						<li>
 							<a
 								target="blank"
-								className="transition duration-200 hover:opacity-70"
+								className="flex justify-start items-center  hover:opacity-70 transition duration-200"
 								href="https://github.com/Davisamasoa"
 							>
-								<i className="bi bi-github bg-transparent"></i>
+								<BsGithub size={21} className="bg-transparent" />
 								<span className="ml-2 ">GitHub</span>
 							</a>
 						</li>
